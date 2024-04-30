0d5ceff59c80eb42c38928211ba5080ee4ca568d
Giovanna Cardim <giovannacardim.profissional@gmail.com>
2024-04-30 11:50:43 -0300

    Feito em sala de aula usando queue na FATEC BS

--- Fila/Fila/Fila.cpp
+++ Fila/Fila/Fila.cpp
@@ -78,22 +78,45 @@ void inicializar()
 void insere()
 {
 	// aloca memoria dinamicamente para o novo elemento
-	NO* novo = (NO*)malloc(sizeof(NO));
-	if (novo == NULL)
+	NO* novo = (NO*)malloc(sizeof(NO)); //1o: Criamos um espaço na memoria para armazenar o novo elemento na fila
+	if (novo == NULL) //2o verifica se não há memoria disponivel
 	{
 		return;
 	}
 
-	cout << "Digite o elemento: ";
-	cin >> novo->valor;
-	novo->prox = NULL;
+	cout << "Digite o elemento: "; //3o pede para o usuario inserir
+	cin >> novo->valor; //4o Armazena o elemento inserido na variavel valor  do nó NOVO
+	novo->prox = NULL; // 5o colocando o PROX como null pois é o ultimo da fila	
+
+	if (inicio == NULL) { //se a fila estiver vazia
+		inicio = novo; // novo é o primeiro
+		fim = novo; // e o ultimo
+		// portanto colocamos o elemento na fila
+	}
+	else {
+		// se ja tem elemento na fila, adicionamos o novo ao final da fila
+		fim->prox = novo; //Adicionar o novo no final da fila 
+		fim = novo; // O ultimo é o elemento que é novo
+	}
 
 
 }
 
 void remove()
 {
+	if (inicio == NULL) { //Se a lista for vazia
+		cout << "Lista vazia. \n";
+	}
 
+	NO* aux = inicio; // coloca o inicio em uma variavel aux
+
+	inicio = inicio->prox; //proximo vai ser armazenado no inicio
+
+	free(aux); //liberar memoria
+	cout << "Elemento removido. \n";
+	if (inicio == NULL) { //verifica se tem algum elemento na fila
+		fim = NULL;
+	}
 
 
 }
